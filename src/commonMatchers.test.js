7c5a9db84443a46dbbb54199b9e65954a3c31a51
johee <johee.chung@gmail.com>
2019-01-30 22:03:54 -0800

    Mock timers.

--- src/commonMatchers.test.js
+++ src/commonMatchers.test.js
@@ -13,26 +13,28 @@ test('toBe', () => {
   expect(2 + 2).toBe(4);
 });
 
-// `.toBeNull` matches only null
-// `.toBeUndefined` matches only undefined
-// `.toBeDefined` is the opposite of toBeUndefined
-// `.toBeTruthy` matches anything that an if statement treats as true
-// `.toBeFalsy` matches anything that an if statement treats as false
-
-test('null', () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
-});
-
-test('zero', () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
+describe('value check', () => {
+  // `.toBeNull` matches only null
+  // `.toBeUndefined` matches only undefined
+  // `.toBeDefined` is the opposite of toBeUndefined
+  // `.toBeTruthy` matches anything that an if statement treats as true
+  // `.toBeFalsy` matches anything that an if statement treats as false
+  
+  test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  
+  test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  });
 });
