8ff057044610611020a4a77c5cd256d27c446462
Johee Chung <johee.chung@nike.com>
2019-01-31 19:16:35 -0800

    Pending, advance, and clear timers.

--- src/mockTimerRun.test.js
+++ src/mockTimerRun.test.js
@@ -19,4 +19,65 @@ describe('running timers', () => {
     expect(callback).toBeCalled();
     expect(callback).toHaveBeenCalledTimes(1);
   });
+
+  test('running times queued to execute within a given time', () => {
+    jest.useFakeTimers();
+    const callback = jest.fn();
+
+    nonRecursiveSetTimeout(callback);
+
+    // Callback should not have been called yet
+    expect(callback).not.toBeCalled();
+
+    // Fast-forward by passed in time (ms), until all timers are executed
+    jest.advanceTimersByTime(1000);
+
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
+
+function recursiveSetTimeout(callback) {
+  setTimeout(() => {
+    callback && callback();
+    setTimeout(() => {
+      recursiveSetTimeout(callback);
+    }, 10000);
+  }, 1000);
+}
+
+describe('recursively run timers', () => {
+  const callback = jest.fn();
+  test('run initial timer', () => {
+    jest.useFakeTimers();
+    recursiveSetTimeout(callback);
+
+    // Should have called setTimeout
+    // at least once by now
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+  });
+
+  test('run proceeding timer + clear pending timers', () => {
+    // Fast forward and exhaust only currently pending timers
+    // but not any new timers that get created during that process
+    // to prevent infinite calls
+    jest.runOnlyPendingTimers();
+
+    // 1 second timer should have fired
+    expect(callback).toBeCalled();
+
+    // 10 second timer should have been initiated
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 10000);
+
+    // Clear pending timers
+    jest.clearAllTimers();
+
+    // Fast-forward by passed in time (ms), until all timers are executed
+    jest.advanceTimersByTime(10000);
+
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+  });
+
 });
